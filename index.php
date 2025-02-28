141479b34a173737573d6aa71d9556473c9e4074
blarkin13 <larki.brian@gmail.com>
2025-02-28 11:17:09 -0600

    cxv

--- index.php
+++ index.php
@@ -1,124 +1,113 @@
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Console</title>
     <style>
-@font-face {
-            font-family: 'Roboto';
-            src: url('/../Stat_Goblin/fonts/roboto/Roboto-Regular.ttf') format('ttf'),
-            url('/../Stat_Goblin/fonts/roboto/Roboto-Regular.ttf') format('ttf');
-            font-weight: normal;
-            font-style: normal;
-            }
-            @font-face {
-            font-family: 'Griffy';
-            src: url('/../Stat_Goblin/fonts/Griffy/Griffy-Regular.ttf') format('ttf'),
-            url('/../Stat_Goblin/fonts/Griffy/Griffy-Regular.ttf') format('ttf');
-            font-weight: normal;
-            font-style: normal;
-            }
-            @font-face {
-            font-family: 'Comfortaa';
-            src: url('/../Stat_Goblin/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf'),
-            url('/../Stat_Goblin/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf');
-            font-weight: normal;
-            font-style: normal;
-            }
-            /* Global Styles */
-            body, html {
-            font-family: 'Comfortaa', sans-serif;
+    <style>
+      @font-face {
+      font-family: 'Roboto';
+      src: url('/../Stat_Goblin/fonts/roboto/Roboto-Regular.ttf') format('ttf'),
+      url('/../Stat_Goblin/fonts/roboto/Roboto-Regular.ttf') format('ttf');
+      font-weight: normal;
+      font-style: normal;
+      }
+      @font-face {
+      font-family: 'Griffy';
+      src: url('/../Stat_Goblin/fonts/Griffy/Griffy-Regular.ttf') format('ttf'),
+      url('/../Stat_Goblin/fonts/Griffy/Griffy-Regular.ttf') format('ttf');
+      font-weight: normal;
+      font-style: normal;
+      }
+      @font-face {
+      font-family: 'Comfortaa';
+      src: url('/../Stat_Goblin/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf'),
+      url('/../Stat_Goblin/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf');
+      font-weight: normal;
+      font-style: normal;
+      }
+      /* Global Styles */
+      body, html {
+      font-family: 'Comfortaa', sans-serif;
       margin: 0;
       padding: 0;
       background: #222;
       color: #eee;
       line-height: 1.5;
       text-align: center;
-    }
-    .containerOuter {
+      }
+      .containerOuter {
       background-color: #333;
       border-bottom: 1px solid #444;
       width: 100%;
       padding: 1rem;
       box-sizing: border-box;
-    }
-    .container {
+      }
+      .container {
       max-width: 800px;
       margin: auto;
-    }
-
-
-    /* Grid layout for dropdowns: 2 per row */
-    .grid-container {
+      }
+      /* Grid layout for links */
+      .grid-container {
       display: grid;
-      grid-template-columns: repeat(2, 1fr);
       grid-gap: 1rem;
       margin-bottom: 1rem;
-    }
-    .grid-item {
+      grid-template-columns: repeat(2, 1fr); /* Two columns by default */
+      }
+      /* For wider screens, switch to 5 columns */
+      @media (min-width: 768px) {
+      .grid-container {
+      grid-template-columns: repeat(5, 1fr);
+      }
+      }
+      .grid-item {
       display: flex;
       flex-direction: column;
-    }
-    .grid-item label {
+      }
+      .grid-item label {
       margin-bottom: 0.3rem;
       font-weight: bold;
       font-size: 1rem;
-    }
-
-
-
-        .logo {
+      }
+      .logo {
       width: 400px;
       display: block;
       margin: 0 auto 1rem auto;
-    }
-    .icon{width:80px}
-  </style>
-
+      }
+      .icon {
+      width: 80px;
+      }
+    </style>
+    </style>
   </head>
-
-<body>
-  <div class="containerOuter">
-    <div class="container">
-      <img src="images/theStatOwl.png" class="logo" alt="Logo">
-      
-      <!-- 2x2 Grid for Dropdowns -->
-      <div class="grid-container">
-        <div class="grid-item">
-          <label for="eventDropdown"><strong>The Scout Owl:</strong></label>
-         <a href ="scouter.php"><img class="icon" src="icons/scouter.png" alt="scouter app"></a>
+  <body>
+    <div class="containerOuter">
+      <div class="container">
+        <img src="images/theStatOwl.png" class="logo" alt="Logo">
+        <!-- 2x2 Grid for Dropdowns -->
+        <div class="grid-container">
+          <div class="grid-item">
+            <label for="eventDropdown"><strong>The Scout Owl:</strong></label>
+            <a href ="scouter.php"><img class="icon" src="icons/scouter.png" alt="scouter app"></a>
+          </div>
+          <div class="grid-item">
+            <label for="eventDropdown"><strong>Owl Admin Console:</strong></label>
+            <a href ="admin-console/"><img class="icon" src="icons/admin.png" alt="Admin Console"></a>
+          </div>
+          <div class="grid-item">
+            <label for="eventDropdown"><strong>Owl Tv:</strong></label>
+            <a href ="dashboard/tv.php"><img class="icon" src="icons/tv.png" alt="Admin Console"></a>
+          </div>
+          <div class="grid-item">
+            <label for="eventDropdown"><strong>Owl Analytics:</strong></label>
+            <a href ="dashboard/"><img class="icon" src="icons/analytics.png" alt="scouter app"></a>
+          </div>
+          <div class="grid-item">
+            <label for="eventDropdown"><strong>Owl Upload:</strong></label>
+            <a href ="admin-console/schedule.php"><img class="icon" src="icons/upload.png" alt="scouter app"></a>
+          </div>
         </div>
-
-
-        <div class="grid-item">
-          <label for="eventDropdown"><strong>Owl Admin Console:</strong></label>
-          
-
- <a href ="admin-console/"><img class="icon" src="icons/admin.png" alt="Admin Console"></a>
-
-
-        </div>
-
-                <div class="grid-item">
-          <label for="eventDropdown"><strong>Owl Analytics:</strong></label>
-         
-
-          <a href ="dashboard/matchstats.php"><img class="icon" src="icons/analytics.png" alt="scouter app"></a>
-
-        </div>
-
-                <div class="grid-item">
-          <label for="eventDropdown"><strong>Upload Schedule:</strong></label>
-          
- <a href ="admin-console/schedule.php"><img class="icon" src="icons/upload.png" alt="scouter app"></a>
-
-
-
-        </div> 
-
-
       </div>
     </div>
-  </div>
-</body>
+  </body>
 </html>
